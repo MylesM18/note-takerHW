e224ab33a3f87465bfdbf3866e962f0aaa2cf052
Myles Magee <mylesmagee562@gmail.com>
2020-05-06 00:14:56 -0700

    delete button works now

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,20 @@ router.get('/api/note', (req,res)=>{
     db.query('SELECT * FROM notes').then(data=>res.json(data))
 })
 
+router.delete('/api/note/:id', (req,res) =>{
+    console.log(req.params.id)
+    db.query('DELETE FROM notes WHERE id = ?', [req.params.id], function(err, result){
+        if (err) {
+            // If an error occurred, send a generic server failure
+            return res.status(500).end();
+          }
+          else if (result.affectedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+          }
+          res.status(200).end();
+      
+    });
+});
+
 module.exports = router;
